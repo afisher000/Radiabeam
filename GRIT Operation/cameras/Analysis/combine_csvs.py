e63d99ed59e583f098c9ef544115bf96d18d0a09
afisher000 <43121839+afisher000@users.noreply.github.com>
2024-08-12 09:44:14 -0700

    updates

---
+++ GRIT Operation/cameras/Analysis/combine_csvs.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+import os
+
+# Specify directory of csvs (saved from series)
+quadscan_dir = '../Scans/2024-08-09/Quadscan'
+
+# Read into dataframes
+dfs = []
+paths = [os.path.join(quadscan_dir, file) for file in os.listdir(quadscan_dir)]
+dfs = [pd.read_csv(path) for path in paths]
+
+# Combine and save to single csv file 
+df = pd.concat(dfs, axis=0).reset_index(drop=True)
+df.to_csv(quadscan_dir + '.csv', index=False)
